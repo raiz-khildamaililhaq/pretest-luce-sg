69ddc88b01b73f45dff58f282ecf64092f5a0027
Khilda maililhaq <khildamaililhaq@pop-os.localdomain>
2025-05-15 11:59:16 +0700

    updte note

--- your_player.rb
+++ your_player.rb
@@ -2,11 +2,15 @@ require './base_player.rb'
 
 class YourPlayer < BasePlayer
   def next_point(time:)
-    # Implement your strategy here.
-    max = grid.max_col + 1
+    # My Stratergy is to walk on continious path from bottom left to bottom right and 
+    # continue up to upper right (+1 col) and came back left and continue
+    # because it use integer data type it automaticaly round down every row itteration
+    # and flip collumn itteration when change
 
-    row = time / max
-    col = even?(row) ? time % max : ( grid.max_col - (time % max) ) 
+    grid_size = grid.max_col + 1
+
+    row = time / grid_size
+    col = even?(row) ? time % max : ((grid_size -1) - (time % max)) 
 
     {
       row: row ,
@@ -14,11 +18,12 @@ class YourPlayer < BasePlayer
     }
   end
 
-  def even?(num)
-    (num % 2 ) == 0
-  end
-
   def grid
     game.grid
+  end
+  
+  private
+  def even?(num)
+    (num % 2 ) == 0
   end
 end
